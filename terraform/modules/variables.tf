eecf084eb00dade9ca69a25b5f0c5ad547a6ec5b
nonzeronucleus <gitkraken@ianplumb.org>
2019-07-20 20:29:49 +0100

    "init"

---
+++ terraform/modules/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  default     = "us-east-1"
+}
+
+variable "domain" {
+  default = "ianplumb.uk"
+  //  default = "ianplumb.uk"
+
+}
+
+variable "subdomain" {
+  default = "fnar"
+  # default = "tftest2"
+}
